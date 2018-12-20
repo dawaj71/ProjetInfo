3fe78e379149038e57001d4a767e1667bd11d2fb
jb <jawad.belhadj@etu.imt-lille-douai.fr>
2018-12-20 16:59:14 +0100

    Application frontend V1

---
+++ app/src/main/java/com/example/jb/frontend/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.jb.frontend;
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.lang.CharSequence;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+    final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button boutton = (Button) findViewById(R.id.button_id);
+        boutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] byteArray = stream.toByteArray();
+            Intent in1 = new Intent(this, SecondActivity.class);
+            in1.putExtra("image",byteArray);
+            startActivity(in1);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/jb/frontend/SecondActivity.java
@@ -0,0 +1,47 @@
+package com.example.jb.frontend;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class SecondActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secondactivity);
+        ImageView mImageView = (ImageView) findViewById(R.id.image);
+        byte[] byteArray = getIntent().getByteArrayExtra("image");
+        Bitmap imageBitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        mImageView.setImageBitmap(imageBitmap);
+
+        Button dkeep = (Button) findViewById(R.id.button2);
+        Button keep = (Button) findViewById(R.id.button);
+
+
+        keep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //envoyer l'image par commande POST au serveur
+            }
+        });
+        dkeep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in2 = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(in2);
+            }
+        });
+
+        }
+
+    }
+
+
