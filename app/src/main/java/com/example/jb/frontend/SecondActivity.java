dc1406b220452c42ab0257b79733b8513a4908f5
jb <jawad.belhadj@etu.imt-lille-douai.fr>
2019-02-04 14:39:05 +0100

    Changement de la méthode de passage de l'image entre 2 activités

--- app/src/main/java/com/example/jb/frontend/SecondActivity.java
+++ app/src/main/java/com/example/jb/frontend/SecondActivity.java
@@ -1,28 +1,47 @@
 package com.example.jb.frontend;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SecondActivity extends AppCompatActivity {
-
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.secondactivity);
         ImageView mImageView = (ImageView) findViewById(R.id.image);
+        final String URL ="http://192.168.43.159:8000/img_searches";
       //  byte[] byteArray = getIntent().getByteAExtra("image");
        // Bitmap imageBitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
-        Bitmap imageBitmap = getIntent().getParcelableExtra("Bitmap");
-        mImageView.setImageBitmap(imageBitmap);
-
+        bitmap = getIntent().getParcelableExtra("Bitmap");
+        mImageView.setImageBitmap(bitmap);
         Button dkeep = (Button) findViewById(R.id.button2);
         Button keep = (Button) findViewById(R.id.button);
 
@@ -31,8 +50,43 @@ public class SecondActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //envoyer l'image par commande POST au serveur
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] imageBytes = baos.toByteArray();
+                final String imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+                StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String s) {
+                        if(s.equals("true")){
+                            Toast.makeText(SecondActivity.this, "Uploaded Successful", Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                            Toast.makeText(SecondActivity.this, "Some error occurred!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        Toast.makeText(SecondActivity.this, "Some error occurred -> "+volleyError, Toast.LENGTH_LONG).show();;
+                    }
+                }) {
+                    //adding parameters to send
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> parameters = new HashMap<String, String>();
+                        parameters.put("img64", imageString);
+                        parameters.put("name", "MyImage");
+                        return parameters;
+                    }
+                };
+
+                RequestQueue rQueue = Volley.newRequestQueue(SecondActivity.this);
+                rQueue.add(request);
             }
         });
+
+
+
         dkeep.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
