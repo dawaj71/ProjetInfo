f48e487a970c88fa97b21a87ac30509dddac0701
Samuel <samuel.lhote@etud.u-picardie.fr>
2019-02-04 09:25:30 +0100

    Commit 9:25 methode get post

--- Backend/views.py
+++ Backend/views.py
@@ -4,11 +4,21 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
+from rest_framework.views import APIView
 
-@csrf_exempt
-@require_POST
-def reception_image(request):
-    request.POST.get()
-    emplacement_image = "local"
-    return JsonResponse({"Success":True},{"Location":emplacement_image})
+class ImageSearche(APIView) :
+    def get(self):
+        print("OK")
+        return 1
+
+    def post(self):
+        print("KO")
+        return 1
+
+    @csrf_exempt
+    @require_POST
+    def reception_image(request):
+        #request.POST.get()
+        emplacement_image = "local"
+        return JsonResponse({"Success":True},{"Location":emplacement_image})
 
